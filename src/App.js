0cdef9cd3c0d5080a00a7dd3b3ca5991a1199c27
meghanakurapati99 <135608282+meghanakurapati99@users.noreply.github.com>
2023-11-30 16:40:32 -0600

    Add files via upload
    Made changes in APP.js for Travel Companion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './components/pages/Home';
-import { Routes, Route } from 'react-router-dom';
-import Services from './components/pages/Services';
-import SignUp from './components/pages/SignUp';
-import Destination from './components/pages/Destination';
-import ScrollToTop from './components/ScrollToTop';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import TravelCompanion from "./components/pages/TravelCompanion";
+import SignUp from "./components/pages/SignUp";
+import CreateAccount from './components/pages/CreateAccount';
+import Destination from "./components/pages/Destination";
+import TravelBuddy from "./components/pages/TravelBuddy"; 
+import ScrollToTop from "./components/ScrollToTop";
+import PaymentForm from "./components/pages/PaymentForm";
+import CurrencyConverter from "./components/pages/CurrencyConverter";
+import MapPage from "./Page/MapPage";
+import { Routes, Route, useLocation } from "react-router-dom";
+import "./App.css";
 
 function App() {
+
+  const location = useLocation();
+
   return (
-    <div className='App'>
+    <div className="App">
       <Navbar />
       <ScrollToTop>
         <Routes>
-          <Route path='/' exact element={<Home />} />
-          <Route path='/services' exact element={<Services />} />
-          <Route path='/sign-up' exact element={<SignUp />} />
-          <Route path="/TravelCompanion" exact element={<TravelCompanion />} />
-          <Route path='/services/activity' element={<Destination />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/TravelCompanion" element={<TravelCompanion />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/CreateAccount" element={<CreateAccount />} />
+          <Route path="/services/activity" element={<Destination />} />
+          <Route path="/Map/:id" element={<MapPage />} />
+          <Route path="/TravelBuddy" element={<TravelBuddy />} /> 
+          <Route path='/destination' element={<Destination />} />
+          <Route path='/PaymentForm' element={<PaymentForm price={location.state?.price} />} />
+          <Route path='/CurrencyConverter' element={<CurrencyConverter />} />
         </Routes>
       </ScrollToTop>
     </div>
